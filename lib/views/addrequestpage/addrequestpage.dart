4c6eb6cb7fd0a79938bee84552cb3e295172b0cf
Aituns <austinbennett03@gmail.com>
2022-02-19 00:02:53 -0600

    fixed issues

--- lib/views/addrequestpage/addrequestpage.dart
+++ lib/views/addrequestpage/addrequestpage.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class AddRequestPage extends StatefulWidget {
   const AddRequestPage({Key? key}) : super(key: key);
@@ -17,10 +16,6 @@ class _AddRequestPageState extends State<AddRequestPage> {
   final profileList = FirebaseFirestore.instance.collection('UserData');
 
   final GlobalKey<FormState> _formKey = GlobalKey();
-
-  Future _updateRequests(String uid, String request) async {
-    return await profileList.doc(uid).update({'Request': request});
-  }
 
   Widget _buildName() {
     return TextFormField(
